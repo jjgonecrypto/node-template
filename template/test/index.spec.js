b6cea375e1108eccd00b225594b271870835f61a
justin j. moses <justinjmoses@gmail.com>
2016-11-12 14:17:52 -0500

    Updating tests

--- template/test/index.spec.js
+++ template/test/index.spec.js
@@ -1,9 +1,11 @@
 const assert = require('assert')
 
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1,2,3].indexOf(4))
-        })
+const {{nameInCode}} = require('../')
+
+describe('{{name}}', function() {
+    describe('#indexOf()', () => {
+        it('works', () => {
+            assert({{nameInCode}}, '{{nameInCode}} must be truthy')
+        });
     })
 })
